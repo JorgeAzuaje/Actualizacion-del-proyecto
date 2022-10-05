9d84961ef9d522b49e3767ee0a7779322e930f77
DAW-DUCKASWELL <114958225+DAW-DUCKASWELL@users.noreply.github.com>
2022-10-05 17:58:53 -0400

    controladores_equipo
    Se creo el controlador para los equipos y se le añadio una función con protocolo post para agregar un nuevo equipo. En este archivo se seguiran metiendo el resto de funciones.

--- controladores/controladores_equipo.js
+++ controladores/controladores_equipo.js
@@ -15,3 +15,18 @@ controladorEquipos.get('/:equipoId', (req,res) => {
     }
     res.json(resultados);
 })
+
+////////////////// - Agregar un nuevo Equipo - POST - //////////////////
+
+controladorEquipos.post('/post', (req, res) => {
+    let equipoNuevo = {
+        "equipoId": "3",
+        "nombEquipo": "Toshiba",
+        "descripcion": "Laptop Negra",
+        "serial": "15IF89AEP98",
+        "fecha_ini": "04/04/2022",
+        "fecha_ult": "09/11/2022",
+        "fecha_man": "03/10/2022"
+    };
+    res.json(equipos);
+});
